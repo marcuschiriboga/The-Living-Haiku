1705dc81c0dc04b70509a996ea0307fc4030b120
Wes <wesleySalesberry@hotmail.com>
2020-07-08 15:23:54 -0400

    inital commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 5000;
+
+connectDB();
+
+app.get('/', (req, res) => res.send('API Running'));
+
+//Sign Up
+app.use('/api/users', require('./routes/api/users'));
+//Sign In
+//app.use('/api/auth', require('./routes/api/auth'));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
