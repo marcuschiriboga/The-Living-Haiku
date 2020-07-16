9b309ff839bae9bb92eb117672525dedcd1d848a
Marcus Chiriboga <marcuschiriboga@gmail.com>
2020-07-15 20:35:59 -0400

    firebase v.2

---
+++ FronEnd/the-living-haiku/src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firebase-auth";
+import "firebase/firebase-database";
+import "firebase/firebase-storage";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAbciB5iYgfj4LXWJXoQUHEQL8gW6oM1ps",
+  authDomain: "the-living-haiku.firebaseapp.com",
+  databaseURL: "https://the-living-haiku.firebaseio.com",
+  projectId: "the-living-haiku",
+  storageBucket: "the-living-haiku.appspot.com",
+  messagingSenderId: "1013068810879",
+  appId: "1:1013068810879:web:8db3117709a5bff8c10d4a",
+  measurementId: "G-S7C7265WXH",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
