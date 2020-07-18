b677471c54fa81da3e05b8c52b2b39d0f55a9a1b
Marcus Chiriboga <marcuschiriboga@gmail.com>
2020-07-18 11:07:35 -0400

    firebaseui

--- FronEnd/the-living-haiku/src/Components/Login.js
+++ FronEnd/the-living-haiku/src/Components/Login.js
@@ -1,38 +1,81 @@
-import React from "react"
-import 'semantic-ui-css/semantic.min.css'
-import { Button, Checkbox, Form } from 'semantic-ui-react'
+import React from "react";
+import "semantic-ui-css/semantic.min.css";
+import { Button, Checkbox, Form } from "semantic-ui-react";
+var firebase = require("../firebase");
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
 
-class Login extends React.Component{
-    constructor(props){
-        super(props);
-
-        this.state = {
-
+    this.state = {};
+  }
+  initApp = () => {
+    firebase.auth().onAuthStateChanged(
+      function (user) {
+        if (user) {
+          // User is signed in.
+          var displayName = user.displayName;
+          var email = user.email;
+          var emailVerified = user.emailVerified;
+          var photoURL = user.photoURL;
+          var uid = user.uid;
+          var phoneNumber = user.phoneNumber;
+          var providerData = user.providerData;
+          user.getIdToken().then(function (accessToken) {
+            document.getElementById("sign-in-status").textContent = "Signed in";
+            document.getElementById("sign-in").textContent = "Sign out";
+            document.getElementById("account-details").textContent = JSON.stringify(
+              {
+                displayName: displayName,
+                email: email,
+                emailVerified: emailVerified,
+                phoneNumber: phoneNumber,
+                photoURL: photoURL,
+                uid: uid,
+                accessToken: accessToken,
+                providerData: providerData,
+              },
+              null,
+              "  ",
+            );
+          });
+        } else {
+          // User is signed out.
+          document.getElementById("sign-in-status").textContent = "Signed out";
+          document.getElementById("sign-in").textContent = "Sign in";
+          document.getElementById("account-details").textContent = "null";
         }
-    }
+      },
+      function (error) {
+        console.log(error);
+      },
+    );
+  };
 
-    render(){
-        return (
-            <Form action="/" method="POST" >
-                <Form.Field>
-                    <label>Name</label>
-                    <input placeholder='Name' name="name" type="text"/>
-                </Form.Field>
-                <Form.Field>
-                    <label>Email</label>
-                    <input placeholder='Email' name="email" type="text"/>
-                </Form.Field>
-                <Form.Field>
-                    <label>Password</label>
-                    <input placeholder='Password' name="password" type="text"/>
-                </Form.Field>
-                <Form.Field>
-                    <Checkbox label='I agree to the Terms and Conditions' />
-                </Form.Field>
-                <Button type='submit'>Submit</Button>
-            </Form>
-        )
-    }
+  render() {
+    return (
+      <>
+        <div id="firebaseui-auth-container">ooo</div>
+        <div id="sign-in-status"></div>
+        <div id="sign-in"></div>
+        <pre id="account-details"></pre>
+        <br />
+        <Form action="/api/auth" method="POST">
+          <Form.Field>
+            <label>Email</label>
+            <input placeholder="Email" name="email" type="text" />
+          </Form.Field>
+          <Form.Field>
+            <label>Password</label>
+            <input placeholder="Password" name="password" type="text" />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox label="I agree to the Terms and Conditions" />
+          </Form.Field>
+          <Button type="submit">Submit</Button>
+        </Form>
+      </>
+    );
+  }
 }
 
 export default Login;
--- FronEnd/the-living-haiku/src/Screens/HomeScreen/HomeScreen.js
+++ FronEnd/the-living-haiku/src/Screens/HomeScreen/HomeScreen.js
@@ -1,34 +1,32 @@
-import React from 'react';
-import 'semantic-ui-css/semantic.min.css'
-import './HomeScreen.css'
-import { Divider, Container} from 'semantic-ui-react'
-import RandomHaikuGen from '../../Components/RandomHaikuGen'
-import LeftSideBar from '../../Components/LeftSideBar'
-import SignInBar from '../../Components/navigation/SignInBar'
-import NavBar from '../../Components/navigation/NavBar/NavBar';
-
+import React from "react";
+import "semantic-ui-css/semantic.min.css";
+import "./HomeScreen.css";
+import { Divider, Container } from "semantic-ui-react";
+import RandomHaikuGen from "../../Components/RandomHaikuGen";
+import LeftSideBar from "../../Components/LeftSideBar";
+import SignInBar from "../../Components/navigation/SignInBar";
+import NavBar from "../../Components/navigation/NavBar/NavBar";
+import Login from "../../Components/Login";
 class HomeScreen extends React.Component {
-    constructor(props){
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
+    this.state = {};
+  }
 
-        }
-    }
-
-    render(){
-        return (
-            <Container >
-                <NavBar/>
-                <Divider />
-                <Container id="homeContainer">
-                    <LeftSideBar />
-                    <RandomHaikuGen />
-                </Container>
-            </Container>
-            
-        )
-    }
+  render() {
+    return (
+      <Container>
+        <NavBar />
+        <Divider />
+        <Container id="homeContainer">
+          <LeftSideBar />
+          <RandomHaikuGen />
+        </Container>
+        <Login />
+      </Container>
+    );
+  }
 }
 
 export default HomeScreen;
