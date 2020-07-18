b0dd769e5a0576034e6308667595aca8ba6b63da
Wes <wesleySalesberry@hotmail.com>
2020-07-18 16:23:13 -0400

    removed context api

--- FronEnd/the-living-haiku/src/App.js
+++ FronEnd/the-living-haiku/src/App.js
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import Axios from 'axios';
-import setAuthToken from './utlis/setAuthToken';
+import React from 'react';
 
 import Login from './Components/Login';
 import MenuBar from './Components/navigation/MenuBar';
@@ -9,55 +7,19 @@ import Register from './Components/auth/Register';
 //import RegisterScreen from './Screens/RegisterScreen/RegisterScreen';
 //import MenuBar from './Components/navigation/MenuBar';
 
-//Context Api for state Start
-import UserContext from './context/UserContext';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 //Context Api for state End
 
 export default function App() {
-	// 	const [ userData, setUserData ] = useState({
-	// 		token: undefined,
-	// 		user: undefined,
-	// 		isAuthenticated: false,
-	// 		loading: true
-	// 	});
-
-	// 	useEffect(() => {
-	// 		setAuthToken(localStorage.token);
-	// 		const checkLoggedIn = async () => {
-	// 			let token = localStorage.getItem('x-auth-token');
-	// 			if (token === null) {
-	// 				localStorage.setItem('x-auth-token', '');
-	// 				token = '';
-	// 			}
-	// 			if (token) {
-	// 				const userRes = await Axios.get('http://localhost:5000/api/auth', {
-	// 					header: { 'x-auth-token': token }
-	// 				});
-
-	// 				setUserData({
-	// 					token,
-	// 					user: userRes.data,
-	// 					isAuthenticated: true
-	// 				});
-
-	// 			}
-	// 		};
-	// 		checkLoggedIn();
-	// 	}, []);
-
 	return (
 		<div className="App">
 			<BrowserRouter>
-				{/* Put everything that will need state between UserContext.Provider  */}
-				<UserContext.Provider>
-					<MenuBar />
-					<Register />
-					<Switch>
-						<Route exact path="/login" component={Login} />
-					</Switch>
-				</UserContext.Provider>
+				<MenuBar />
+				<Register />
+				<Switch>
+					<Route exact path="/login" component={Login} />
+				</Switch>
 			</BrowserRouter>
 		</div>
 	);
