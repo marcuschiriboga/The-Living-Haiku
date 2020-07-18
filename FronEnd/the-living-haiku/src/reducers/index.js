e90dac2b1ad1afb932212659309b0260f807aae9
Timothy Reynoso <domex2467@gmail.com>
2020-07-18 17:37:34 -0400

    connected the combined reducers to the store

--- FronEnd/the-living-haiku/src/reducers/index.js
+++ FronEnd/the-living-haiku/src/reducers/index.js
@@ -2,7 +2,9 @@ import { combineReducers } from 'redux';
 import auth from './auth';
 import profile from './profile';
 
-export default combineReducers({
+const allReducers =  combineReducers({
 	auth,
 	profile
 });
+
+export default allReducers;
