f764247676f38f5a9f4f4015d0d6c6b4b1d03c58
Marcus Chiriboga <marcuschiriboga@gmail.com>
2020-07-19 15:38:14 -0400

    randomized Poems

--- FronEnd/the-living-haiku/src/Components/RandomHaikuGen.js
+++ FronEnd/the-living-haiku/src/Components/RandomHaikuGen.js
@@ -2,11 +2,14 @@ import React from "react";
 import "semantic-ui-css/semantic.min.css";
 import { Card, Icon, Button } from "semantic-ui-react";
 import { poemlist } from "../PoemList";
+import firebase from "../firebase";
 class RandomHaikuGen extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      poemsList: {},
+      poemsArray: [],
       stanza1: "",
       stanza2: "",
       stanza3: "",
@@ -16,6 +19,7 @@ class RandomHaikuGen extends React.Component {
       // TODO: generate random poems from the poems in state
     };
   }
+
   buildRandomPoem = (poemlist) => {
     this.setState({
       stanza1: poemlist[0][0],
@@ -23,8 +27,44 @@ class RandomHaikuGen extends React.Component {
       stanza3: poemlist[2][Math.round(Math.random() * poemlist[2].length)],
     });
   };
+  componentDidMount() {
+    let getData = () => {
+      firebase
+        .database()
+        // .ref(`/users/${userId}/poems/`)
+        .ref(`allpoems/`)
+        .once("value")
+        .then((snapshot) => {
+          var poems = snapshot.val();
 
+          this.setState({
+            poemsList: poems,
+          });
+          let poemsArray = [];
+          for (let keys in this.state.poemsList) {
+            let newPoem = [
+              this.state.poemsList[keys].title,
+              this.state.poemsList[keys].stanza1,
+              this.state.poemsList[keys].stanza2,
+              this.state.poemsList[keys].stanza3,
+            ];
+            console.log(keys);
+            poemsArray.push(newPoem);
+          }
+          this.setState({
+            poemsArray: poemsArray,
+          });
+          this.setState({
+            stanza1: this.state.poemsArray[Math.floor(Math.random() * this.state.poemsArray.length)][1],
+            stanza2: this.state.poemsArray[Math.floor(Math.random() * this.state.poemsArray.length)][2],
+            stanza3: this.state.poemsArray[Math.floor(Math.random() * this.state.poemsArray.length)][3],
+          });
+        });
+    };
+    getData();
+  }
   render() {
+    // this.getData();
     return (
       <Card>
         <Card.Content>
@@ -38,9 +78,9 @@ class RandomHaikuGen extends React.Component {
           positive
           onClick={() => {
             this.setState({
-              stanza1: poemlist[0][Math.floor(Math.random() * poemlist[0].length)],
-              stanza2: poemlist[1][Math.floor(Math.random() * poemlist[1].length)],
-              stanza3: poemlist[2][Math.floor(Math.random() * poemlist[2].length)],
+              stanza1: this.state.poemsArray[Math.floor(Math.random() * this.state.poemsArray.length)][1],
+              stanza2: this.state.poemsArray[Math.floor(Math.random() * this.state.poemsArray.length)][2],
+              stanza3: this.state.poemsArray[Math.floor(Math.random() * this.state.poemsArray.length)][3],
             });
           }}
         >
