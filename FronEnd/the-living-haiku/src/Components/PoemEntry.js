715de225eade7b8d4429a93df5a15578a70ff8c6
Marcus Chiriboga <marcuschiriboga@gmail.com>
2020-07-19 20:08:24 -0400

    authposting and posting tags

--- FronEnd/the-living-haiku/src/Components/PoemEntry.js
+++ FronEnd/the-living-haiku/src/Components/PoemEntry.js
@@ -1,44 +1,65 @@
 import React from "react";
 import "semantic-ui-css/semantic.min.css";
-import { Button, Checkbox, Form } from "semantic-ui-react";
+import { Button, Checkbox, Form, Dropdown } from "semantic-ui-react";
 import firebase from "../firebase";
+import { connect } from "react-redux";
 
-//redux
-// import {useSelector, useDispatch} from 'react-redux'
-// import { postPoem } from '../actions/poems'
+const options = [
+  { key: "Pets", text: "Pets", value: "pets" },
+  { key: "Nature", text: "Nature", value: "nature" },
+  { key: "love", text: "Love ", value: "love" },
+  { key: "Home", text: "Home", value: "home" },
+  { key: "Identity", text: "Identity", value: "identity" },
+  { key: "Self", text: "Self", value: "self" },
+  { key: "Spiritual", text: "Spiritual", value: "spiritual" },
+  { key: "Toys", text: "Toys", value: "toys" },
+  { key: "Games", text: "Games", value: "Games" },
+  { key: "Time", text: "Time", value: "time" },
+  { key: "Place", text: "Place", value: "place" },
+  { key: "Future", text: "Future", value: "future" },
+  { key: "Food", text: "Food", value: "food" },
+  { key: "Sadness", text: "Sadness", value: "sadness" },
+  { key: "Educational", text: "Educational", value: "Educational" },
+];
 
 class PoemEntry extends React.Component {
   constructor(props) {
     super(props);
-    // TODO: pull userId from context/redux
-    // TODO: properly link up poem database.
+    // TODO:
     this.state = {
       title: "",
       stanza1: "",
       stanza2: "",
       stanza3: "",
+      tags: [],
       errors: [],
       loading: false,
     };
   }
+  isFormValid = ({ userId, poemData }) => userId && poemData;
+  dropdownHandler = (e, data) => {
+    this.setState({ ...this.state, tags: data.value });
+  };
   handleChange = (evt) => this.setState({ [evt.target.name]: evt.target.value });
   handleSubmit = (evt) => {
+    // console.log(evt);
     evt.preventDefault();
-    console.log(this.state);
-    let userId = "DcKwK9kCAnRJrpudqVJS1bj9szh2";
+    // console.log(this.state);
+    let userId = this.props.currentUser.uid;
     let poemData = {
       title: this.state.title,
       stanza1: this.state.stanza1,
       stanza2: this.state.stanza2,
       stanza3: this.state.stanza3,
-      tags: ["educational", "testing"],
+      tags: this.state.tags,
     };
     var newPostKey = firebase.database().ref().child("posts").push().key;
-    console.log(newPostKey);
-
-    console.log(poemData);
-    firebase.database().ref(`users/${userId}/poems/${newPostKey}`).set(poemData);
-    firebase.database().ref(`allpoems/${newPostKey}`).update(poemData);
+    // console.log(newPostKey);
+    if (this.isFormValid({ userId, poemData })) {
+      console.log(poemData);
+      firebase.database().ref(`users/${userId}/poems/${newPostKey}`).set(poemData);
+      firebase.database().ref(`allpoems/${newPostKey}`).update(poemData);
+    }
   };
 
   render() {
@@ -54,25 +75,57 @@ class PoemEntry extends React.Component {
             type="text"
             onChange={this.handleChange}
             value={title}
+            required
           />
         </Form.Field>
         <Form.Field>
           <label>Stanza 1</label>
-          <input placeholder="5 syllables" name="stanza1" type="text" onChange={this.handleChange} value={stanza1} />
+          <input
+            placeholder="5 syllables"
+            name="stanza1"
+            type="text"
+            onChange={this.handleChange}
+            value={stanza1}
+            required
+          />
         </Form.Field>
         <Form.Field>
           <label>Stanza 2</label>
-          <input placeholder="7 syllables" name="stanza2" type="text" onChange={this.handleChange} value={stanza2} />
+          <input
+            placeholder="7 syllables"
+            name="stanza2"
+            type="text"
+            onChange={this.handleChange}
+            value={stanza2}
+            required
+          />
         </Form.Field>
         <Form.Field>
           <label>Stanza 3</label>
-          <input placeholder="5 syllables" name="stanza3" type="text" onChange={this.handleChange} value={stanza3} />
+          <input
+            placeholder="5 syllables"
+            name="stanza3"
+            type="text"
+            onChange={this.handleChange}
+            value={stanza3}
+            required
+          />
         </Form.Field>
+        <Dropdown
+          placeholder="Tag Your Poem for the Generator"
+          fluid
+          multiple
+          selection
+          options={options}
+          onChange={this.dropdownHandler}
+        />
         <Button type="submit">Submit</Button>
-        {/* onSubmit={() => dispatch(postPoem(poemData))} */}
       </Form>
     );
   }
 }
 
-export default PoemEntry;
+const mapStateToProps = (state) => ({
+  currentUser: state.auth.currentUser,
+});
+export default connect(mapStateToProps)(PoemEntry);
