66409a98253f5651a293ac7cd856377e23ca8bff
Marcus Chiriboga <marcuschiriboga@gmail.com>
2020-07-19 22:27:19 -0400

    userPoems allPoems

--- FronEnd/the-living-haiku/src/Components/SortPoemsByTag.js
+++ FronEnd/the-living-haiku/src/Components/SortPoemsByTag.js
@@ -1,10 +1,7 @@
-import React from "react"
+import React from "react";
 import "semantic-ui-css/semantic.min.css";
 import { Divider, Card, Icon, Button, Dropdown, Image } from "semantic-ui-react";
 import firebase from "../firebase";
-
-
-
 
 const options = [
   { key: "Pets", text: "Pets", value: "pets" },
@@ -23,98 +20,95 @@ const options = [
   { key: "Sadness", text: "Sadness", value: "sadness" },
   { key: "Educational", text: "Educational", value: "Educational" },
 ];
-    
-class SortPoemsByTag extends React.Component {
 
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-          poemsList: {},
-          poemsArray: [],
-        };
-            this.getData = () => {
-            let userId = "DcKwK9kCAnRJrpudqVJS1bj9szh2";
-            firebase
-                .database()
-                .ref(`/users/${userId}/poems/`)
-                // .ref(`allpoems/`)
-                .once("value")
-                .then((snapshot) => {
-                var poems = snapshot.val();
-   
-    
-            this.setState({
+class SortPoemsByTag extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      poemsList: {},
+      poemsArray: [],
+    };
+    this.getData = () => {
+      let userId = "DcKwK9kCAnRJrpudqVJS1bj9szh2";
+      firebase
+        .database()
+        .ref(`/users/${userId}/poems/`)
+        // .ref(`allpoems/`)
+        .once("value")
+        .then((snapshot) => {
+          var poems = snapshot.val();
+
+          this.setState({
             poemsList: poems,
-            });
-            let poemsArray = [];
-            for (let keys in this.state.poemsList) {
+          });
+          let poemsArray = [];
+          for (let keys in this.state.poemsList) {
             let newPoem = [
-                this.state.poemsList[keys].title,
-                this.state.poemsList[keys].stanza1,
-                this.state.poemsList[keys].stanza2,
-                this.state.poemsList[keys].stanza3,
-                this.state.poemsList[keys].tag
+              this.state.poemsList[keys].title,
+              this.state.poemsList[keys].stanza1,
+              this.state.poemsList[keys].stanza2,
+              this.state.poemsList[keys].stanza3,
+              this.state.poemsList[keys].tag,
             ];
-            if(this.state.poemsList[keys].tag === options.value){
-                console.log(keys);
-                poemsArray.push(newPoem);}
+            if (this.state.poemsList[keys].tag === options.value) {
+              console.log(keys);
+              poemsArray.push(newPoem);
             }
-            this.setState({
+          }
+          this.setState({
             poemsArray: poemsArray,
-            });
+          });
         });
-        };
-      }
-      //TODO: list poems with a certain list of tags.
-      //TODO: list tags.
-    
-      render() {
-        console.log(this.state.poemsArray);
-        // let randomPoem = poems[0][2];
-        //   poems[Math.floor(Math.random() * poems.length)][3],
-        //   poems[Math.floor(Math.random() * poems.length)][4],
-        // ];
-        // console.log(randomPoem);
-        return (
-          <>
-            {this.state.poemsArray.map((content, index) => {
-              console.log(content);
-              return (
-                <>
-                <Divider />
-                  <Card key={index} index={index}>
-                    <Card.Content header={content[0]} />
-                    <Card.Content>
-                        
-                      <Card.Group>
-                        <Card header={content[1]} />
-                        <Card header={content[2]} />
-                        <Card header={content[3]} />
-                      </Card.Group>
-                    </Card.Content>
-                  </Card>
-                </>
-              );
-            })}
-            <Card>
-              <Card.Content header={`Haiku's By You`} />
-              <Card.Content>
-                <Card.Group>
-                  <Card fluid color="red" header="The days grow longer" />
-                  <Card fluid color="orange" header="The months and years seem to shrink" />
-                  <Card fluid color="yellow" header="I love to eat food" />
-                </Card.Group>
-              </Card.Content>
-              <Dropdown placeholder="Haiku(#Tags)" fluid multiple selection options={options} />
-              <Button onClick={this.getData} positive>
-                Get
-              </Button>
-            </Card>
-          </>
-        );
-      }
-    
+    };
+  }
+  //TODO: list poems with a certain list of tags.
+  //TODO: list tags.
+
+  render() {
+    console.log(this.state.poemsArray);
+    // let randomPoem = poems[0][2];
+    //   poems[Math.floor(Math.random() * poems.length)][3],
+    //   poems[Math.floor(Math.random() * poems.length)][4],
+    // ];
+    // console.log(randomPoem);
+    return (
+      <>
+        {this.state.poemsArray.map((content, index) => {
+          console.log(content);
+          return (
+            <>
+              <Divider />
+              <Card key={index} index={index}>
+                <Card.Content header={content[0]} />
+                <Card.Content>
+                  <Card.Group>
+                    <Card header={content[1]} />
+                    <Card header={content[2]} />
+                    <Card header={content[3]} />
+                  </Card.Group>
+                </Card.Content>
+              </Card>
+            </>
+          );
+        })}
+        <Card>
+          <Card.Content header={`Haiku's By You`} />
+          <Card.Content>
+            <Card.Group>
+              <Card fluid color="red" header="The days grow longer" />
+              <Card fluid color="orange" header="The months and years seem to shrink" />
+              <Card fluid color="yellow" header="I love to eat food" />
+            </Card.Group>
+          </Card.Content>
+          <Dropdown placeholder="Haiku(#Tags)" fluid multiple selection options={options} />
+          <Button onClick={this.getData} positive>
+            Get
+          </Button>
+        </Card>
+      </>
+    );
+  }
 }
 
 export default SortPoemsByTag;
--- FronEnd/the-living-haiku/src/Components/navigation/MenuBar.js
+++ FronEnd/the-living-haiku/src/Components/navigation/MenuBar.js
@@ -4,7 +4,7 @@ import { Image, List } from "semantic-ui-react";
 import "./MenuBar.css";
 //import "./MenuBar.css"
 // import { Login } from "../../Components"
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 import PageNotFound from "../PageNotFound.js";
 
 //import SignUpScreen from "../../Screens/SignUpScreen/SignUpScreen"
@@ -20,8 +20,7 @@ import NavBar from "./NavBar/NavBar";
 import PostPoemsScreen from "../../Screens/PostPoemsScreen/PostPoemsScreen";
 import UserHaikus from "../../Screens/UserHaikus/UserHaikus";
 import GetPoemsByTags from "../../Screens/GetPoemsByTags/GetPoemsByTags";
-import dojo from "../../Picture/dojo.png"
-
+import dojo from "../../Picture/dojo.png";
 
 class MenuBar extends React.Component {
   constructor(props) {
@@ -33,9 +32,7 @@ class MenuBar extends React.Component {
     return (
       <Router>
         <div id="total_page_container">
-          <div className="dojo_container">
-            <img className="dojo" src={dojo} alt="Failed to load" />
-          </div>
+          <div className="dojo_container"></div>
           <div className="title_bar">
             <h1 className="animate__fadeIn">The Living Haiku</h1>
             <h3>生きている俳句</h3>
@@ -100,6 +97,7 @@ class MenuBar extends React.Component {
             <Route exact path="/PostPoems" component={PostPoemsScreen} />
             <Route exact path="/UserHaikus" component={UserHaikus} />
             <Route exact path="/GetPoemsByTags" component={GetPoemsByTags} />
+            <Route exact path="/allHaikus" component={AllPoemsScreen} />
           </Switch>
         </div>
       </Router>
